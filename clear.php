7caa119e0f8c0f3a38ab60672435ebd19d9f1210
Babak Naghiyev <bnagiyev@gmail.com>
2018-08-07 19:18:18 +0400

    Update clear.php

--- clear.php
+++ clear.php
@@ -1,10 +1,17 @@
+function clearXSS($data){
+    $data = htmlentities($data,ENT_QUOTES,'utf-8');
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = strip_tags($data);
+}
+
 function clearPOST($post){
     foreach( $post as $attr => $val ) 
     {
         if(is_array($val)){
             $post[$attr] = clearPOST($val);
         } else {
-            $post[$attr] = Yii::$app->base->clearXcc($val);
+            $post[$attr] = clearXSS($val);
         }
     }
     return $post;
