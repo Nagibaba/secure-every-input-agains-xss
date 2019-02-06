8f5c7438818f4bf171c56fc8e321768c1b91f570
Babak Naghiyev <bnagiyev@gmail.com>
2019-02-06 11:19:00 +0300

    Update clear.php

--- clear.php
+++ clear.php
@@ -1,3 +1,10 @@
+// *** I found a better way  *** //
+// @ clears all POST and GET inputs against XSS attack
+$_GET   = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+// *** below is very detailed. *** //
+
+
 function clearXSS($data){
     $data = htmlentities($data,ENT_QUOTES,'utf-8');
     $data = htmlspecialchars($data);
